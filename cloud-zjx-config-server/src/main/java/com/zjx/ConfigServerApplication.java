3808be1fd70166e7bcc6e9bf866375d8e4a8ba32
zhengjiaxing <zjx_jacky@163.com>
2018-11-16 16:31:17 +0800

    高可用config

--- cloud-zjx-config-server/src/main/java/com/zjx/ConfigServerApplication.java
+++ cloud-zjx-config-server/src/main/java/com/zjx/ConfigServerApplication.java
@@ -3,6 +3,7 @@ package com.zjx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * http请求地址和资源文件映射如下:
@@ -19,6 +20,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
